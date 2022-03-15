ef1998838a4b7384a285baab9013b09b35855127
Paulo Adeva <paulo.adeva@gmail.com>
2022-03-15 20:55:20 +1100

    small fixes

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+const toggle = document.querySelector('.toggle');
+
+
 function lightOrDark() {
     const root = document.documentElement;
     const newTheme = root.className === 'light' ? 'dark' : 'light';
     root.className = newTheme;
-
 }
+
 
 lightOrDark()
 
-document.querySelector('.toggle').addEventListener('click', lightOrDark)
+toggle.addEventListener('click', lightOrDark)
+
